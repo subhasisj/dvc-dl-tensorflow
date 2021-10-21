75c513bb2a0410c1cb0476ef8d4db3c9c275d687
subhasisj <monty.rulz@gmail.com>
2021-10-22 02:10:25 +0530

    Added Params and Main Stage 02 file

--- src/stage-02-prepare-base-model.py
+++ src/stage-02-prepare-base-model.py
@@ -7,8 +7,8 @@ from tqdm import tqdm
 
 from src.utils.utils import copy_files, create_directory, read_yaml
 
-log_directory = os.path.join(os.getcwd(), "logs")
-os.makedirs(log_directory, exist_ok=True)
+log_directory = os.path.join(os.getcwd(),"logs")
+os.makedirs(log_directory,exist_ok=True)
 logging_str = "[%(asctime)s: %(levelname)s: %(module)s:] %(message)s"
 log_filename = "stage-logs.log"
 logging.basicConfig(
@@ -19,27 +19,32 @@ logging.basicConfig(
 )
 
 
-def get_data(path_to_config):
+def prepare_base_model(path_to_config,path_to_params):
     config = read_yaml(path_to_config)
-    remote_data_source = config["data"]["source_path"]
-    local_data_source = config["data"]["local_path"]
+    params = read_yaml(path_to_params)
 
-    # Download Data from source_path to local_path
-    for src_dir, target_dir in tqdm(
-        zip(remote_data_source, local_data_source), total=2, colour="blue"
-    ):
-        create_directory([target_dir])
-        copy_files(src_dir, target_dir)
+    artifacts_dir = config["artifacts"]["DIR"]
+    base_model_dir = config["artifacts"]["BASE_MODEL_DIR"]
 
+    # Create base model directory
+    base_model_dir_path = os.path.join(artifacts_dir,base_model_dir)
+    create_directory([base_model_dir_path])
+
+    model_name = config["artifacts"]["BASE_MODEL_NAME"]
+    model_path = os.path.join(base_model_dir_path,model_name)
+
+    model = get_vgg16(input_shape = params["IMAGE_SIZE"])
+    
 
 if __name__ == "__main__":
     args = argparse.ArgumentParser()
     args.add_argument("--config", "-c", default="config/config.yaml")
+    args.add_argument("--params", "-p", default="params.yaml")
     parsed_args = args.parse_args()
 
     try:
         logging.info(" >>>>>> Starting Stage 01")
-        get_data(parsed_args.config)
+        prepare_base_model(parsed_args.config,parsed_args.params)
         logging.info("Stage 01 completed successfully >>>>>>")
     except Exception as e:
         logging.exception(e)
